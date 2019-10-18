1f58b695c30be342ea94de7a848cd00dc16f65d0
Cary Orange <caryo@yahoo.com>
2019-10-18 07:47:55 -0400

    moved roll maprand genrand onbase offbase result_code result statrun stat and side to engine1.c

---
+++ hbg/engine1.h
@@ -0,0 +1,47 @@
+#ifndef ENGINE1_H
+#define ENGINE1_H
+
+struct bstatdata {
+   int pa;
+   int ab;
+   int r;
+   int h;
+   int rbi;
+   int bb;
+   int so;
+   int lob;
+   int s;
+   int d;
+   int t;
+   int hr;
+   int gdp;
+   int hbp;
+   int sf;
+};
+
+struct pstatdata {
+   int ip;
+   int ip_f;
+   int w;
+   int l;
+};
+
+struct probdata {
+   int p[12];
+};
+
+int    roll(void);
+int    maprand(int p[], int n, int z0);
+int    genrand(int p[], int n);
+void   onbase(int ob[], int li);
+int    offbase(int ob[]);
+char * result_code(int z);
+int    result(int z, int a, int *o, int *h, int *r, int *c, int *gdp, int idx, int ob[], int rli[], int *rlo);
+void   statrun(struct bstatdata stats[], int idx, int result);
+void   stat(struct bstatdata stats[], int idx, int result, int gdp, int rbi, int lob);
+void   side(struct probdata p[], int n,
+            int i, int b, int d, int *r, int *h, int *li,
+            int li_base, struct bstatdata batstat[], struct bstatdata pitbstat[],
+            struct pstatdata pitpstat[], int pitIdx, int *lob);
+
+#endif
--- hbg/readme.txt
+++ hbg/readme.txt
@@ -1,4 +1,5 @@
 # gcc hbg*.c -o hbg
 # gcc -DUSE_DICE -DDEBUG -Wall -Wstrict-prototypes -ansi -pedantic -o engine engine.c
 gcc -Wall -Wstrict-prototypes -ansi -pedantic -c engine0.c
-gcc -Wall -Wstrict-prototypes -ansi -pedantic -o engine engine.c engine0.o
+gcc -Wall -Wstrict-prototypes -ansi -pedantic -c engine1.c
+gcc -Wall -Wstrict-prototypes -ansi -pedantic -o engine engine.c engine0.o engine1.o
